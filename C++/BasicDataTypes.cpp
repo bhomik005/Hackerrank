25c4bd85b6343626f6e2c21c950bb03a7c9b0d92
bhomik005 <bhomik005@gmail.com>
2024-12-16 18:24:24 +1100

    BasicDataTypes.cpp

---
+++ C++/BasicDataTypes.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int i;
+    long l;
+    char ch;
+    float f;
+    double d;
+    // using printf and scanf in C++
+    scanf("%d %ld %c %f %lf", &i, &l, &ch, &f, &d);
+    printf("%d\n%ld\n%c\n%f\n%lf", i, l, ch, f, d);
+    return 0;
+}
